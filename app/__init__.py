d54d12257476e74f6e3fbf4d7d0f2a3058ca0036
AlinaJackie <alina.baran.22@pnu.edu.ua>
2024-12-14 15:55:23 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt  # Змінив FlaskBcrypt на Bcrypt
+from flask_bcrypt import Bcrypt 
 from flask_login import LoginManager
 from flask_migrate import Migrate
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config.from_object('app.config.Config')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-bcrypt = Bcrypt(app)  # Використовуємо Bcrypt
+bcrypt = Bcrypt(app) 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
