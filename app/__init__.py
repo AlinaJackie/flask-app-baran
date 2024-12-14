a980d56dfedd914e3385252883e48e8838ae0742
Your Name <you@example.com>
2024-12-14 15:54:18 +0100

    Added styles and improvements to user-related templates

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt  # Змінив FlaskBcrypt на Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+app = Flask(__name__)
+app.config.from_object('app.config.Config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)  # Використовуємо Bcrypt
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from app import routes, models
+
